42571fd7bb75c2ae3371005414189cfc141a9188
marcellogoiaba <marcellogoaiba@gmail.com>
2018-11-14 14:20:19 +0000

    added angular routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { StoreComponent } from '../app/components/store/store.component';
+import { ProductDetailComponent } from '../app/components/product-detail/product-detail.component';
+import { ContactComponent } from '../app/components/contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/store', pathMatch: 'full' },
+  { path: 'store', component: StoreComponent },
+  { path: 'productDetail', component: ProductDetailComponent },
+  { path: 'contact' , component: ContactComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
